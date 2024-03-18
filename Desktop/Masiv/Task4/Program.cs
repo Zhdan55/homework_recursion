32093128fce7c1e4cdf6f3b0d1cce0b34813d45b
Zhdan55 <tosha.zhdanov.2000@gmail.com>
2024-03-18 13:52:38 +0700

    Add new task

---
+++ Desktop/Masiv/Task4/Program.cs
@@ -0,0 +1,10 @@
+﻿
+double CalculateFormula(int a, int b, int c, int d)
+{
+    double num = a * b;
+    int denum = c + d;
+    double result = num / denum;
+    return result;
+}
+double result=CalculateFormula(1, 2, 3, 4);
+Console.WriteLine(result);
