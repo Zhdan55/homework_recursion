07198ea2ba764df86ef8e0af68ffedf4cc2aee4f
Zhdan55 <tosha.zhdanov.2000@gmail.com>
2024-03-21 12:50:44 +0700

    Add new task

---
+++ Desktop/Function_homework/Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая бесконечно запрашивает целые числа с консоли. 
+//Программа завершается при вводе символа ‘q’ или при вводе числа, сумма цифр которого чётная.
+
+tring input = " ";
+    int num;
+    bool exit = true;
+    while (exit)
+    {
+        input = Input();
+        if (int.TryParse(input, out num))
+            exit = !(num % 2 == 0);
+        else
+        if (input == "q")
+            exit = false;
+    }
+    string Input()
+    {
+        Console.WriteLine("Введите число или символ 'q' для выхода");
+        return Console.ReadLine();
+    }
+
