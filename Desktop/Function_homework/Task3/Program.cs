f2f4d8b99729b6998bbb2be35c69a1e01af4a760
Zhdan55 <tosha.zhdanov.2000@gmail.com>
2024-03-21 13:25:24 +0700

    Add Task3

---
+++ Desktop/Function_homework/Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, 
+//которая перевернёт одномерный массив (первый элемент станет последним, второй – предпоследним и т.д.)
+public class Example
+{
+    public static void Main()
+    {
+        int[] array = { 2, 4, 6, 8 };
+        Array.Reverse(array);
+ 
+        Console.WriteLine(String.Join(',', array));
+    }
+}
