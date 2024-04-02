c7151e227c16dc7d3666b5c3adc5345d46bc32b0
Zhdan55 <tosha.zhdanov.2000@gmail.com>
2024-04-02 18:52:48 +0700

    Add new Tasks

---
+++ Desktop/String/Task2/Program.cs
@@ -0,0 +1,35 @@
+﻿// На основе символов строки (тип string) сформировать массив
+// символов (тип char[]). Вывести массив на экран.
+// Указание
+// Метод строки ToCharArray() не использовать.
+// Пример
+// “Hello!” => [‘H’, ‘e’, ‘l’, ‘l’, ‘o’, ‘!’ ]
+
+void Main()
+{
+System.Console.Write("Введите строку: ");
+string? str = Console.ReadLine();
+
+// char[] c = str!.ToCharArray();
+// PrintArray(c);
+
+char[] chars = StringToCharArray(str);
+PrintArray(chars);
+}
+
+char[] StringToCharArray(string? str)
+{
+char[] tempChars = new char[str!.Length];
+
+for(int i = 0; i < tempChars.Length; i++)
+tempChars[i] = str[i];
+
+return tempChars;
+}
+
+void PrintArray(char[] chars)
+{
+System.Console.WriteLine("['" + string.Join("', '", chars) + "']");
+}
+
+Main();
